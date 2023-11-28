b1899bb2ba65a807af2f22d218dc4874f0a297b4
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2023-11-28 19:36:11 +0530

    Update page.tsx

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -33,7 +33,7 @@ export default function Applications() {
 
     useEffect(() => {
         fetchApplications();
-    });
+    }, []);
 
     type OrganizationType = 'civil' | 'chapter' | 'public';
 
